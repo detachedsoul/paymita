db4555f63f16974b2e79d8e7dd43e0fb36d361b0
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2024-01-28 21:06:50 +0100

    chore: fixed hero section text wrap

--- components/Index/HeroSection.tsx
+++ components/Index/HeroSection.tsx
@@ -6,13 +6,13 @@ import DownloadAndroid from "@/assets/img/download-android.svg";
 
 const HeroSection = (): JSX.Element => {
     return (
-        <div className="grid items-center bg-[#e1f3ff] scroll-mt-14 lg:scroll-mt-[4.5rem] lg:grid-cols-12" id="about">
-            <div className="py-8 px-4 grid gap-4 lg:col-span-5 lg:pl-20 lg:py-0 lg:pr-0">
+        <div className="grid items-center bg-[#e1f3ff] scroll-mt-14 lg:scroll-mt-[4.5rem] lg:grid-cols-2" id="about">
+            <div className="py-8 px-4 grid gap-4 lg:pl-20 lg:py-0 lg:pr-0">
                 <h1 className="text-[2.5rem] text-brand-dark-purple font-bold leading-[110%] lg:text-[3.5rem]">
                     Your Gateway to Effortless & Swift Payments.
                 </h1>
 
-                <p>
+                <p className="text-wrap">
                     Experience unparalleled simplicity in managing your financial transactions — from converting airtime to cash to seamless bill payments and betting wallet funding.
                 </p>
 
@@ -27,7 +27,7 @@ const HeroSection = (): JSX.Element => {
                 </div>
             </div>
 
-            <div className="lg:col-span-7">
+            <div>
                 <Image src={IndexHeroImage} alt="Paymita - Your Gateway to Effortless & Swift Payments." priority fetchPriority="high" />
             </div>
         </div>
